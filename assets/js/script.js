88be957bbc8f1ebbdd2930640f02ce3f643849ac
Hector Fernandez <hkfernandezdev@gmail.com>
2021-01-13 08:52:17 -0600

    recent cities array created and new cites are being added

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,11 +39,15 @@ when user click on a recent city
         $("<input>")
             .attr ("id", "searchBar")
             .attr ("placeholder", "City Name")
-            .append (
-                $("<button>")
-                    .text("save")
-            )
-    );
+
+
+     )
+     $("#citySelectionPane").append(
+             $("<button>")
+                .attr("id", "searchBtn")
+                 .text("search")
+     )
+ 
     $("#citySectionPane").append(
         $("<div>")
             .attr ("id", "recentCitiesPane")
@@ -66,3 +70,58 @@ when user click on a recent city
             .text ("future conditions pane")
     );
 
+var APIKey = "40a8eac704499a683458b2a328507962"
+var recentCities =[""]
+
+function pullRecentCitiesLocalStorage (){
+    recentCities = localStorage.getItem (JSON.parse("weatherRecentCiteiesArr"));
+}
+
+function addCityToRecentCitites (userInputCity) {
+    if (!recentCities.includes(userInputCity)) {
+        if (recentCities < 8) {
+            recentCities.unshift(userInputCity);
+        } else {
+            recentCities.splice(0,1,userInputCity)
+        }
+    }
+}
+
+// on click search - calls getCityInfo() which calls addCityToCurrentCities()
+$("#searchButtn").on("click", getCityInfo);
+
+function getCityInfo () {
+    // var userInput = $("#searchBar").val();
+    var userInput = "Raleigh,NC"
+    var address = 'api.openweathermap.org/data/2.5/forecast?q=Minneapolis&appid=40a8eac704499a683458b2a328507962';
+    var callParameters = "";
+    // $("#searchBar").val("");
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Minneapolis,MN&appid=" + APIKey;
+ 
+addCityToRecentCitites (userInput)
+console.log(recentCities);
+
+// if ()
+
+
+    $.ajax({
+        url: address,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+      });
+
+
+}
+
+//     $.ajax({
+//         type: "GET",
+//         url: address,
+//         success: function (response) {
+//             console.log (response);
+//         }
+//     });   
+// };
+
+
+getCityInfo ();
