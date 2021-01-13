f77a9f9294f7cea4ee2735dde1be7f3f9eb67808
Hector Fernandez <hkfernandezdev@gmail.com>
2021-01-12 18:42:47 -0600

    nitial layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,68 @@
-alert ("connected");
+/*
+$ - $create link to jQuery
+$ - create basic structure
+    header
+    sidebar
+        input
+        list of cities searched
+    main
+        current conditions
+        future conditions
+when user enters a city and clicks on search
+    city is stored into recents array
+    city is set to current city
+    weather is dislayed for current city
+when user click on a recent city
+    city is set to current city
+    weather is displayed for curent city
+
+*/
+
+// STRUCTURE LAYOUT
+    // header
+    $("#mainContainer").append(
+        $("<header>")
+            .attr("id","header")
+            .text("header")
+    );
+    // aside w/ search bar and list of recent cities
+    $("#mainContainer").append(
+        $("<aside>")
+            .attr("id", "citySelectionPane")
+            .text("city selection pane")
+            .append(
+                $("<div>")
+                    .text("Search for a City:")
+            )
+    );
+    $("#citySelectionPane").append(
+        $("<input>")
+            .attr ("id", "searchBar")
+            .attr ("placeholder", "City Name")
+            .append (
+                $("<button>")
+                    .text("save")
+            )
+    );
+    $("#citySectionPane").append(
+        $("<div>")
+            .attr ("id", "recentCitiesPane")
+            .text ("recent cities")
+    )
+    // main content w/ current conditions and future conditions
+    $("#mainContainer").append(
+        $("<main>")
+            .attr ("id", "conditionsPane")
+            .text ("conditions pane")
+            .append (
+                $("<section>")
+                    .attr ("id", "currentConditionsPane")
+                    .text("current conditions pane")
+            )
+    );
+    $("#conditionsPane").append(
+        $("<section>")
+            .attr ("id", "futureConditions")
+            .text ("future conditions pane")
+    );
+
